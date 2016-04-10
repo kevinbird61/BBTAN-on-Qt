470373a875e70ef606dd90605b12843bfedc8659
kevinbird61 <kevinbird61@gmail.com>
2016-04-10 14:59:43 +0800

    Complete tutorial one.
    Rename BBTAN -> BBTAN-tutorial one.

--- BBtan-Tutoial(1)/scene.h
+++ BBtan-Tutoial(1)/scene.h
@@ -14,13 +14,20 @@ public:
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     /* Variable */
     Btn *btn_start;
+    Btn *btn_change_ball;
+    Btn *btn_pause;
+    Btn *btn_conti;
+    Btn *btn_back;
     int btn_w;
     int btn_h;
+    int btn_small_w;
+    int btn_small_h;
     // Use to control the scene page
     int screenMode;
 
 public slots:
     void bgChange();
+    void bgRecover();
 
 };
 
